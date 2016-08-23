258e57b9fb85f4f07dddaeebbaff60ee00cf45d6
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-08-23 22:15:20 +0200

    added new clock for tracking cumulative game time

--- src/doredo/entities.py
+++ src/doredo/entities.py
@@ -4,6 +4,8 @@ import pyglet.gl as gl
 
 class Entity(object):
 
+    dynamic = False
+
     def __init__(self, verts, x=0., y=0., color=(255, 255, 255)):
         self.verts = verts
         self.color = color
@@ -12,13 +14,18 @@ class Entity(object):
         self._vertex_list = None
 
     @property
+    def dynamic(self):
+        return False
+
+    @property
     def vertex_list(self):
         if type(self._vertex_list) != type(None):
             return self._vertex_list
         else:
             n_verts = int(len(self.verts) / 2)
+            update_mode = 'dynamic' if self.dynamic else 'static'
             self._vertex_list = graphics.vertex_list(3, #int(n_verts),
-                                                     ('v2f/dynamic', self.verts),
+                                                     ('v2f/{}'.format(update_mode), self.verts),
                                                      ('c3B', self.color * n_verts)
                                                      )
             return self._vertex_list
@@ -26,3 +33,26 @@ class Entity(object):
 
     def draw(self, mode=gl.GL_TRIANGLES):
         self.vertex_list.draw(mode=mode)
+
+
+    def update(self, dt):
+        pass
+
+class AnimatedEntity(Entity):
+
+    dynamic = True
+
+    def __init__(self, *args, anim_fun=None, anim_args=tuple(), **kwargs):
+        super(AnimatedEntity, self).__init__(*args, **kwargs)
+        self.anim_fun = anim_fun
+        self.anim_args = tuple(anim_args)
+
+
+    def update(self, dt):
+        super(AnimatedEntity, self).update(dt)
+        if self.anim_fun:
+            self.vertex_list.vertices[:] = self.anim_fun(self.vertex_list.vertices, *self.anim_args)
+
+
+
+
