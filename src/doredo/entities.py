12775cdc55cca531f344ce8654a06b2b9894b44c
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-08-24 17:32:12 +0200

    added color setting feature

--- src/doredo/entities.py
+++ src/doredo/entities.py
@@ -6,10 +6,8 @@ import numpy as np
 
 class Entity(object):
 
-    def __init__(self, verts, x=0., y=0., color=(255, 255, 255)):
-        self.vertex_list = self._build_vertex_list(verts)
-
-        self.color = color
+    def __init__(self, verts, x=0., y=0., color=(1., 1., 1.)):
+        self.vertex_list = self._build_vertex_list(verts, color)
         self.x = x
         self.y = y
         self.__keyframe = 0.
@@ -32,6 +30,19 @@ class Entity(object):
         self.vertex_list.normals[:] = verts
 
     @property
+    def color(self):
+        return self.vertex_list.colors[:3]
+
+    @color.setter
+    def color(self, values):
+        n_verts = int(len(self.vertex_list.vertices) / 3)
+        if len(values) / 3 == n_verts:
+            self.vertex_list.colors[:] = values
+        else:
+            self.vertex_list.colors[:] = values * n_verts
+
+
+    @property
     def keyframe(self):
         return self.__keyframe
 
@@ -40,14 +51,14 @@ class Entity(object):
         assert 0. <= value <= 1.
         self.__keyframe = value
 
-    def _build_vertex_list(self, verts):
+    def _build_vertex_list(self, verts, color=(.1, .1, .5)):
         vv = np.array(verts).ravel()
         n_verts = int(vv.size / 3)
         update_mode = 'dynamic'
 
         vertex_list = graphics.vertex_list(n_verts,
                                                  ('v3f/{}'.format(update_mode), vv),
-                                                 # ('c3B', self.color * n_verts),
+                                                 ('c3f', color * n_verts),
                                                  ('n3f/{}'.format(update_mode), vv),
                                                  )
         return vertex_list
--- src/runapp.py
+++ src/runapp.py
@@ -12,7 +12,7 @@ triangle_verts = (-1., -1.,
                   0., 1.)
 triangle_verts = utils.add_depth(triangle_verts)
 
-player = Entity(triangle_verts)
+player = Entity(triangle_verts, color=(1., 0., .5))
 player.normals = np.array(player.vertices) * -1
 # new_verts = [np.sin(cumclock.cumtime) * v for v in triangle_verts]
 
@@ -21,20 +21,27 @@ vert = """
 #version 330 core
 layout(location = 0) in vec3 vert0;
 layout(location = 2) in vec3 vert1;
+layout(location = 3) in vec3 col;
+
+out vec3 col2;
 
 uniform float keyframe;
 void main(){
 
     gl_Position.xyz = mix(vert0, vert1, keyframe);
     gl_Position.w = 1.0;
+
+    col2 = col;
 }
 """
 
 frag = """
 #version 330 core
 out vec3 color;
+in vec3 col2;
 void main(){
-  color = vec3(1,0,0);
+//  color = vec3(1,0,0);
+    color = col2;
 }
 """
 
@@ -53,5 +60,6 @@ def update(dt):
 
 
 pyglet.clock.schedule(update)
+
 
 pyglet.app.run()
