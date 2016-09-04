fixed rotation and scaling

src/doredo/entities.py
@@ -32,12 +32,23 @@ class Physical(object):
 
     def update(self):
         x, y, s, rot = self.x, self.y, self.scale, self.rot
-        mm = np.array([[s * np.cos(rot), -np.sin(rot), 0, x],
-                       [np.sin(rot),  s * np.cos(rot), 0, y],
-                       [              0,            0, s, 0],
-                       [              0,            0, 0, 1]
-                      ])
-        self.modelmat = mm
+        trans = np.array([[s, 0, 0, x],
+                          [0, s, 0, y],
+                          [0, 0, s, 0],
+                          [0, 0, 0, 1.]], dtype=float)
+
+        rot = np.array([[np.cos(rot), -np.sin(rot), 0, 0],
+                       [np.sin(rot), np.cos(rot), 0, 0],
+                       [0, 0, 1, 0],
+                       [0, 0, 0, 1]], dtype=float)
+
+        scale = np.array([[s, 0, 0, 0],
+                          [0, s, 0, 0],
+                          [0, 0, s, 0],
+                          [0, 0, 0, 1]], dtype=float)
+
+        # self.modelmat = np.dot(scale, np.dot(rot, trans))
+        self.modelmat = np.dot(trans, np.dot(rot, scale))
 
 
 

src/runapp.py
@@ -14,6 +14,7 @@ triangle_verts = utils.add_depth(resources.triangle)
 
 player = Entity(triangle_verts, color=(1., 0., .5))
 player.normals = np.array(player.vertices) * -1
+player.obj.scale = .2
 # player.obj.x = .7
 
 # new_verts = [np.sin(cumclock.cumtime) * v for v in triangle_verts]
@@ -60,7 +61,7 @@ def on_draw():
 
 def update(dt):
     player.update(dt)
-    player.keyframe = .5 * np.sin(cumclock.cumtime) + 0.5
+    # player.keyframe = .5 * np.sin(cumclock.cumtime) + 0.5
     player.obj.rot += np.pi * dt
 
 