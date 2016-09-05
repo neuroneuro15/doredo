ee292818aa9302a32d8ddf42270a7e44f389cf51
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2016-09-05 19:31:46 +0200

    fixed rotation and scaling

--- src/doredo/resources.py
+++ src/doredo/resources.py
@@ -1,6 +1,7 @@
 import os
 from os import path
 from .shader import Shader
+import numpy as np
 
 triangle = (-1., -1.,
             1., -1.,
@@ -12,6 +13,13 @@ square = (-1., 1.,
          -1., -1.,
           1., -1.,
           1.,  1.)
+
+def get_ngon_vertices(sides):
+    assert sides > 3
+    angles = np.linspace(0, 2 * np.pi / sides
+
+
+
 
 shader_path = path.join(path.split(__file__)[0], 'shaders')
 
